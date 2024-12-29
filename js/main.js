432d0a955c4851c15e51220088224da6a19f1282
Hafida3412 <lounsy81@gmail.com>
2024-12-29 21:52:51 +0100

    main.js add shadow header

--- js/main.js
+++ js/main.js
@@ -36,3 +36,10 @@ const myAtropos = Atropos({
 });
 
 /*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    //Add a class if the bottom offset is greater than 50
+    this.scrollY >= 50 ? header.classList.add(shadow-header)
+                       : header.classList.remove(shadow-header)
+}
+window.addEventListener('scroll', shadowHeader)
